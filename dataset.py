3016dd6e63d1acd834e346cc29d9e12347d19cfa
Rahul-144 <77679404+Rahul-144@users.noreply.github.com>
2025-01-02 10:42:37 +0530

    Updates

--- dataset.py
+++ dataset.py
@@ -4,20 +4,22 @@ from PIL import Image
 from torchvision import transforms
 
 class ImagetoImageDataset(Dataset):
-    def __init__(self, domainA_dir, domainB_dir, transform=None):
+    def __init__(self, domainA_dir, domainB_dir,domainC_dir, transform=None):
         self.domainA_images = [os.path.join(domainA_dir, x) for x in os.listdir(domainA_dir) if x.endswith('.png') or x.endswith('.jpg')]
         self.domainB_images = [os.path.join(domainB_dir, x) for x in os.listdir(domainB_dir) if x.endswith('.png') or x.endswith('.jpg')]
+        self.domainC_images = [os.path.join(domainC_dir, x) for x in os.listdir(domainB_dir) if x.endswith('.png') or x.endswith('.jpg')]
         self.transform = transform
 
     def __len__(self):
-        return min(len(self.domainA_images), len(self.domainB_images))
+        return min(len(self.domainA_images), len(self.domainB_images),len(self.domainC_images))
 
     def __getitem__(self, idx):
         img_A = Image.open(self.domainA_images[idx]).convert('RGB')
         img_B = Image.open(self.domainB_images[idx]).convert('RGB')
-
+        img_C = Image.open(self.domainC_images[idx]).convert('RGB')
         if self.transform:
             img_A = self.transform(img_A)
             img_B = self.transform(img_B)
+            img_C = self.transform(img_C)
 
-        return img_A, img_B
+        return img_A, img_B, img_C
--- train.py
+++ train.py
@@ -1,117 +1,179 @@
 import torch
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
+import torch.nn.functional as F
 from models.generator import Generator
 from models.discriminator import Discriminator
 from dataset import ImagetoImageDataset
 from config import config
 import itertools
-class AgingGAN(pl.LightningModule):
+from torchvision import transforms
 
+class AgingGAN(pl.LightningModule):
     def __init__(self, hparams):
         super(AgingGAN, self).__init__()
         self.save_hyperparameters(hparams)
-        self.genA2B = Generator(hparams['ngf'], n_residual_blocks=hparams['n_blocks'])
-        self.genB2A = Generator(hparams['ngf'], n_residual_blocks=hparams['n_blocks'])
-        self.disGA = Discriminator(hparams['ndf'])
-        self.disGB = Discriminator(hparams['ndf'])
         self.automatic_optimization = False
-        # cache for generated images
-        self.generated_A = None
-        self.generated_B = None
-        self.real_A = None
-        self.real_B = None
 
-    def forward(self, x):
-        return self.genA2B(x)
+        # Generators for all transitions
+        self.genYoung2Middle = Generator(hparams['ngf'], n_residual_blocks=hparams['n_blocks'])
+        self.genMiddle2Young = Generator(hparams['ngf'], n_residual_blocks=hparams['n_blocks'])
+        self.genMiddle2Old = Generator(hparams['ngf'], n_residual_blocks=hparams['n_blocks'])
+        self.genOld2Middle = Generator(hparams['ngf'], n_residual_blocks=hparams['n_blocks'])
+        
+        # Discriminators for each age group
+        self.disYoung = Discriminator(hparams['ndf'])
+        self.disMiddle = Discriminator(hparams['ndf'])
+        self.disOld = Discriminator(hparams['ndf'])
+
+        # Cache for generated images
+        self.generated_middle_from_young = None
+        self.generated_young_from_middle = None
+        self.generated_old_from_middle = None
+        self.generated_middle_from_old = None
+        self.real_young = None
+        self.real_middle = None
+        self.real_old = None
+
+    def train_dataloader(self):
+        transform = transforms.Compose([
+            transforms.Resize(256),
+            transforms.RandomCrop(256),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+        ])
+        
+        dataset = ImagetoImageDataset(
+            domainA_dir=config['trainYoung_dir'],
+            domainB_dir=config['trainMiddle_dir'],
+            domainC_dir=config['trainOld_dir'],
+            transform=transform
+        )
+        
+        return DataLoader(
+            dataset,
+            batch_size=self.hparams['batch_size'],
+            shuffle=True,
+            num_workers=4,
+            pin_memory=True
+        )
 
     def training_step(self, batch, batch_idx):
-      g_opt, d_opt = self.optimizers()
-      real_A, real_B = batch
+        g_opt, d_opt = self.optimizers()
+        real_young, real_middle, real_old = batch
 
-      # Generator Training
-      g_opt.zero_grad()
-      
-      # Identity loss
-      same_B = self.genA2B(real_B)
-      loss_identity_B = F.l1_loss(same_B, real_B) * self.hparams['identity_weight']
-      same_A = self.genB2A(real_A)
-      loss_identity_A = F.l1_loss(same_A, real_A) * self.hparams['identity_weight']
+        # Generator Training
+        g_opt.zero_grad()
+        
+        # Identity losses
+        same_middle_y = self.genYoung2Middle(real_middle)
+        same_young_m = self.genMiddle2Young(real_young)
+        same_old_m = self.genMiddle2Old(real_old)
+        same_middle_o = self.genOld2Middle(real_middle)
+        
+        loss_identity = (
+            F.l1_loss(same_middle_y, real_middle) +
+            F.l1_loss(same_young_m, real_young) +
+            F.l1_loss(same_old_m, real_old) +
+            F.l1_loss(same_middle_o, real_middle)
+        ) * self.hparams['identity_weight']
 
-      # GAN loss
-      fake_B = self.genA2B(real_A)
-      pred_fake = self.disGB(fake_B)
-      loss_GAN_A2B = F.mse_loss(pred_fake, torch.ones(pred_fake.shape).type_as(pred_fake)) * self.hparams['adv_weight']
+        # GAN losses
+        fake_middle_y = self.genYoung2Middle(real_young)
+        fake_young_m = self.genMiddle2Young(real_middle)
+        fake_old_m = self.genMiddle2Old(real_middle)
+        fake_middle_o = self.genOld2Middle(real_old)
 
-      fake_A = self.genB2A(real_B)
-      pred_fake = self.disGA(fake_A)
-      loss_GAN_B2A = F.mse_loss(pred_fake, torch.ones(pred_fake.shape).type_as(pred_fake)) * self.hparams['adv_weight']
+        loss_GAN = (
+            F.mse_loss(self.disMiddle(fake_middle_y), torch.ones_like(self.disMiddle(fake_middle_y))) +
+            F.mse_loss(self.disYoung(fake_young_m), torch.ones_like(self.disYoung(fake_young_m))) +
+            F.mse_loss(self.disOld(fake_old_m), torch.ones_like(self.disOld(fake_old_m))) +
+            F.mse_loss(self.disMiddle(fake_middle_o), torch.ones_like(self.disMiddle(fake_middle_o)))
+        ) * self.hparams['adv_weight']
 
-      # Cycle loss
-      recovered_A = self.genB2A(fake_B)
-      loss_cycle_ABA = F.l1_loss(recovered_A, real_A) * self.hparams['cycle_weight']
+        # Cycle losses
+        recovered_young = self.genMiddle2Young(fake_middle_y)
+        recovered_middle_y = self.genYoung2Middle(fake_young_m)
+        recovered_middle_o = self.genOld2Middle(fake_old_m)
+        recovered_old = self.genMiddle2Old(fake_middle_o)
 
-      recovered_B = self.genA2B(fake_A)
-      loss_cycle_BAB = F.l1_loss(recovered_B, real_B) * self.hparams['cycle_weight']
+        loss_cycle = (
+            F.l1_loss(recovered_young, real_young) +
+            F.l1_loss(recovered_middle_y, real_middle) +
+            F.l1_loss(recovered_middle_o, real_middle) +
+            F.l1_loss(recovered_old, real_old)
+        ) * self.hparams['cycle_weight']
 
-      # Total generator loss
-      g_loss = loss_identity_A + loss_identity_B + loss_GAN_A2B + loss_GAN_B2A + loss_cycle_ABA + loss_cycle_BAB
-      
-      # Manually backward and optimize for generator
-      self.manual_backward(g_loss)
-      g_opt.step()
+        # Total generator loss
+        g_loss = loss_identity + loss_GAN + loss_cycle
+        
+        self.manual_backward(g_loss)
+        g_opt.step()
 
-      # Store generated images for discriminator training
-      self.generated_B = fake_B.detach()
-      self.generated_A = fake_A.detach()
+        # Store generated images
+        self.generated_middle_from_young = fake_middle_y.detach()
+        self.generated_young_from_middle = fake_young_m.detach()
+        self.generated_old_from_middle = fake_old_m.detach()
+        self.generated_middle_from_old = fake_middle_o.detach()
 
-      # Discriminator Training
-      d_opt.zero_grad()
+        # Discriminator Training
+        d_opt.zero_grad()
 
-      # Discriminator A
-      pred_real = self.disGA(real_A)
-      loss_D_real = F.mse_loss(pred_real, torch.ones(pred_real.shape).type_as(pred_real))
-      
-      pred_fake = self.disGA(self.generated_A)
-      loss_D_fake = F.mse_loss(pred_fake, torch.zeros(pred_fake.shape).type_as(pred_fake))
-      
-      loss_D_A = (loss_D_real + loss_D_fake) * 0.5
+        # Young discriminator
+        loss_D_young = (
+            F.mse_loss(self.disYoung(real_young), torch.ones_like(self.disYoung(real_young))) +
+            F.mse_loss(self.disYoung(self.generated_young_from_middle), torch.zeros_like(self.disYoung(self.generated_young_from_middle)))
+        ) * 0.5
 
-      # Discriminator B
-      pred_real = self.disGB(real_B)
-      loss_D_real = F.mse_loss(pred_real, torch.ones(pred_real.shape).type_as(pred_real))
-      
-      pred_fake = self.disGB(self.generated_B)
-      loss_D_fake = F.mse_loss(pred_fake, torch.zeros(pred_fake.shape).type_as(pred_fake))
-      
-      loss_D_B = (loss_D_real + loss_D_fake) * 0.5
+        # Middle discriminator
+        loss_D_middle = (
+            F.mse_loss(self.disMiddle(real_middle), torch.ones_like(self.disMiddle(real_middle))) +
+            F.mse_loss(self.disMiddle(self.generated_middle_from_young), torch.zeros_like(self.disMiddle(self.generated_middle_from_young))) +
+            F.mse_loss(self.disMiddle(self.generated_middle_from_old), torch.zeros_like(self.disMiddle(self.generated_middle_from_old)))
+        ) * 0.5
 
-      # Total discriminator loss
-      d_loss = loss_D_A + loss_D_B
-      
-      # Manually backward and optimize for discriminator
-      self.manual_backward(d_loss)
-      d_opt.step()
+        # Old discriminator
+        loss_D_old = (
+            F.mse_loss(self.disOld(real_old), torch.ones_like(self.disOld(real_old))) +
+            F.mse_loss(self.disOld(self.generated_old_from_middle), torch.zeros_like(self.disOld(self.generated_old_from_middle)))
+        ) * 0.5
 
-      # Log losses
-      self.log('Loss/Generator', g_loss)
-      self.log('Loss/Discriminator', d_loss)
+        d_loss = loss_D_young + loss_D_middle + loss_D_old
+        
+        self.manual_backward(d_loss)
+        d_opt.step()
 
-      return {'loss': g_loss + d_loss}
+        # Log losses
+        self.log('Loss/Generator', g_loss)
+        self.log('Loss/Discriminator', d_loss)
+
+        return {'loss': g_loss + d_loss}
 
     def configure_optimizers(self):
         g_optim = torch.optim.Adam(
-            itertools.chain(self.genA2B.parameters(), self.genB2A.parameters()),
-            lr=self.hparams['lr'], 
-            betas=(0.5, 0.999),
-            weight_decay=self.hparams['weight_decay']
-        )
-        d_optim = torch.optim.Adam(
-            itertools.chain(self.disGA.parameters(), self.disGB.parameters()),
+            itertools.chain(
+                self.genYoung2Middle.parameters(),
+                self.genMiddle2Young.parameters(),
+                self.genMiddle2Old.parameters(),
+                self.genOld2Middle.parameters()
+            ),
             lr=self.hparams['lr'],
             betas=(0.5, 0.999),
             weight_decay=self.hparams['weight_decay']
         )
+        
+        d_optim = torch.optim.Adam(
+            itertools.chain(
+                self.disYoung.parameters(),
+                self.disMiddle.parameters(),
+                self.disOld.parameters()
+            ),
+            lr=self.hparams['lr'],
+            betas=(0.5, 0.999),
+            weight_decay=self.hparams['weight_decay']
+        )
+        
         return [g_optim, d_optim]
 
 # Example command to train
